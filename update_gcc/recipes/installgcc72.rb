143305ca57022ade7b0fb022965b7ab1c953396c
Winston Luu <winston@zsupplyllc.com>
2019-06-28 00:49:43 -0700

    Added GCC recipes

---
+++ update_gcc/recipes/installgcc72.rb
@@ -0,0 +1,4 @@
+# Install gcc72
+execute "yum install gcc72-c++" do
+  command "yum install gcc72-c++"
+end
---
+++ update_gcc/recipes/removegcc48.rb
@@ -0,0 +1,8 @@
+# Remove gcc48.
+execute "yum remove gcc48-c++" do
+  command "yum remove gcc48-c++"
+end
+
+
+
+
